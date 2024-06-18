a25f5390b83189680b179dde74179eee89f80d45
Ch00cy_Developer <chooyeonhee99@gmail.com>
2024-06-18 09:04:53 +0900

    Reapply "backup"
    This reverts commit a7347f303f05c8f0e63dc8448e9e5313a34fcf63.

--- app/static/js/map-tmy_checklist.js
+++ app/static/js/map-tmy_checklist.js
@@ -1,10 +1,11 @@
 // 하위 항목 리스트를 토글하는 함수
 function toggleChildList(id) {
-    var childList = document.getElementById(id);
+    var childList = document.getElementById(id); // 주어진 ID를 가진 요소를 찾습니다.
+    // 현재 하위 항목 리스트의 표시 상태를 토글합니다.
     if (childList.style.display === 'none' || childList.style.display === '') {
-        childList.style.display = 'block';
+        childList.style.display = 'block'; // 하위 리스트를 보이도록 설정
     } else {
-        childList.style.display = 'none';
+        childList.style.display = 'none'; // 하위 리스트를 숨기도록 설정
     }
 }
 
@@ -12,8 +13,9 @@ function toggleChildList(id) {
 function updateSelectedMarkers(checked, fullCode, marker) {
     if (checked) {
         if (!selectedMarkers.includes(fullCode)) {
-            selectedMarkers.push(fullCode);
+            selectedMarkers.push(fullCode); // 선택된 마커 목록에 추가
         }
+        // 마커의 아이콘을 노란색 점으로 변경
         marker.setIcon(L.icon({
             iconUrl: '/static/data/icon/yellow-dot.png',
             iconSize: [32, 32],
@@ -23,32 +25,34 @@ function updateSelectedMarkers(checked, fullCode, marker) {
     } else {
         var index = selectedMarkers.indexOf(fullCode);
         if (index > -1) {
-            selectedMarkers.splice(index, 1);
+            selectedMarkers.splice(index, 1); // 선택된 마커 목록에서 제거
         }
+        // 마커의 아이콘을 기본 아이콘으로 변경
         marker.setIcon(marker.options.defaultIcon);
     }
+    // 선택된 마커들의 이름을 표시
     document.getElementById('markerNames').innerText = selectedMarkers.join(', ');
 }
 
 // 상위 체크박스의 상태를 업데이트하는 함수
 function updateParentCheckboxes() {
-    var parentCheckboxes = document.querySelectorAll('.parent-checkbox');
+    var parentCheckboxes = document.querySelectorAll('.parent-checkbox'); // 모든 상위 체크박스를 찾습니다.
     parentCheckboxes.forEach(function(parentCheckbox) {
-        var childList = parentCheckbox.nextElementSibling.nextElementSibling;
+        var childList = parentCheckbox.nextElementSibling.nextElementSibling; // 상위 체크박스의 다음 다음 요소 (하위 리스트)
         if (childList) {
-            var childCheckboxes = childList.querySelectorAll('input[type="checkbox"]');
-            var allChecked = Array.from(childCheckboxes).every(checkbox => checkbox.checked);
-            var someChecked = Array.from(childCheckboxes).some(checkbox => checkbox.checked);
+            var childCheckboxes = childList.querySelectorAll('input[type="checkbox"]'); // 하위 리스트의 모든 체크박스를 찾습니다.
+            var allChecked = Array.from(childCheckboxes).every(checkbox => checkbox.checked); // 모든 하위 체크박스가 체크되었는지 확인
+            var someChecked = Array.from(childCheckboxes).some(checkbox => checkbox.checked); // 일부 하위 체크박스가 체크되었는지 확인
 
             if (allChecked) {
-                parentCheckbox.indeterminate = false;
-                parentCheckbox.checked = true;
+                parentCheckbox.indeterminate = false; // 체크 상태가 모호하지 않음
+                parentCheckbox.checked = true; // 상위 체크박스를 체크 상태로 설정
             } else if (someChecked) {
-                parentCheckbox.indeterminate = true;
-                parentCheckbox.checked = false;
+                parentCheckbox.indeterminate = true; // 체크 상태가 모호함
+                parentCheckbox.checked = false; // 상위 체크박스를 체크 해제 상태로 설정
             } else {
-                parentCheckbox.indeterminate = false;
-                parentCheckbox.checked = false;
+                parentCheckbox.indeterminate = false; // 체크 상태가 모호하지 않음
+                parentCheckbox.checked = false; // 상위 체크박스를 체크 해제 상태로 설정
             }
         }
     });
@@ -57,65 +61,36 @@ function updateParentCheckboxes() {
 // 모든 상위 체크박스에 이벤트 리스너를 추가
 document.querySelectorAll('.parent-checkbox').forEach(function(parentCheckbox) {
     parentCheckbox.addEventListener('change', function() {
-        var childList = parentCheckbox.nextElementSibling.nextElementSibling;
+        var childList = parentCheckbox.nextElementSibling.nextElementSibling; // 상위 체크박스의 다음 다음 요소 (하위 리스트)
         if (childList) {
-            var childCheckboxes = childList.querySelectorAll('input[type="checkbox"]');
+            var childCheckboxes = childList.querySelectorAll('input[type="checkbox"]'); // 하위 리스트의 모든 체크박스를 찾습니다.
             if (parentCheckbox.checked) {
+                // 상위 체크박스가 체크되면 하위 체크박스들도 모두 체크
                 childCheckboxes.forEach(function(childCheckbox) {
                     childCheckbox.checked = true;
-                    childCheckbox.dispatchEvent(new Event('change'));
+                    childCheckbox.dispatchEvent(new Event('change')); // 상태 변경 이벤트 발생
                 });
             } else {
+                // 상위 체크박스가 체크 해제되면 하위 체크박스들도 모두 체크 해제
                 childCheckboxes.forEach(function(childCheckbox) {
                     childCheckbox.checked = false;
-                    childCheckbox.dispatchEvent(new Event('change'));
+                    childCheckbox.dispatchEvent(new Event('change')); // 상태 변경 이벤트 발생
                 });
             }
         }
-        updateParentCheckboxes();
-    });
-});
-
-// 1_TMY_78 및 3_TMY_22의 상위 체크박스에 대한 이벤트 리스너 추가
-document.querySelectorAll('#1_TMY_78, #3_TMY_22').forEach(function(subCheckbox) {
-    subCheckbox.addEventListener('change', function() {
-        var childList = subCheckbox.nextElementSibling.nextElementSibling;
-        if (childList) {
-            var childCheckboxes = childList.querySelectorAll('input[type="checkbox"]');
-            if (subCheckbox.checked) {
-                childCheckboxes.forEach(function(childCheckbox) {
-                    childCheckbox.checked = true;
-                    childCheckbox.dispatchEvent(new Event('change'));
-                });
-            } else {
-                childCheckboxes.forEach(function(childCheckbox) {
-                    childCheckbox.checked = false;
-                    childCheckbox.dispatchEvent(new Event('change'));
-                });
-            }
-        }
-        updateParentCheckboxes();
     });
 });
 
 // 모든 토글 버튼에 이벤트 리스너를 추가
 document.querySelectorAll('.toggle-button').forEach(function(toggleButton) {
     toggleButton.addEventListener('click', function() {
-        var childList = toggleButton.parentElement.querySelector('.child-list');
+        var childList = toggleButton.parentElement.querySelector('.child-list'); // 토글 버튼의 부모 요소 내 하위 리스트
         if (childList.style.display === 'none' || childList.style.display === '') {
-            childList.style.display = 'block';
-            toggleButton.innerHTML = '▼';
+            childList.style.display = 'block'; // 하위 리스트를 보이도록 설정
+            toggleButton.innerHTML = '▼'; // 버튼 텍스트 변경
         } else {
-            childList.style.display = 'none';
-            toggleButton.innerHTML = '▶';
+            childList.style.display = 'none'; // 하위 리스트를 숨기도록 설정
+            toggleButton.innerHTML = '▶'; // 버튼 텍스트 변경
         }
     });
 });
-
-// 초기화 함수
-function initializeCheckboxes() {
-    updateParentCheckboxes(); // 상위 체크박스 상태 업데이트
-}
-
-// 초기화 함수 호출
-initializeCheckboxes();
--- app/static/js/map-tmy_main.js
+++ app/static/js/map-tmy_main.js
@@ -18,7 +18,59 @@ function addMarkers(folder, subfolder, siteFile) {
                     var markersData = results.data; // 파싱된 데이터 배열
 
                     // 2021_100 폴더에 대한 예외 처리
-                    if (folder === '2021_100') {
+                    if (folder === '2022_200') {
+                        // 2021_100 외의 폴더에 대한 처리
+                        var checklist = document.getElementById(folder + '_list');
+
+                        markersData.forEach(function(markerData, index) {
+                            var lat = parseFloat(markerData.latitude);  // 위도
+                            var lng = parseFloat(markerData.longitude); // 경도
+                            var code = markerData.code || markerData.Code || markerData['Marker Code']; 
+                            var fullCode = folder + '+' + code; // 마커 이름 출력할 때 사용 , 데이터 이름 구분짓기 위해
+
+                            // 유효하지 않은 데이터 -> 건너뜀
+                            if (!lat || !lng || !code) {
+                                console.error('Invalid data: ', markerData);
+                                return;
+                            }
+
+                            // 마커 생성 -> 지도 추가
+                            var marker = L.marker([lat, lng], { 
+                                icon: icons[folder],    // 폴더 안의 아이콘 사용
+                                defaultIcon: icons[folder] // 기본 아이콘 저장
+                            }).addTo(map);
+                            markers[fullCode] = marker; // 마커를 markers 객체에 저장
+
+                            // 체크리스트 항목 생성
+                            var listItem = document.createElement('li');    // 새로운 리스트 항목 (li) 요소 생성
+                            var checkbox = document.createElement('input'); // 새로운 입력 (input) 요소 생성
+                            checkbox.type = 'checkbox'; // 입력 요소의 타임을 체크박스로 설정
+                            checkbox.id = fullCode; // 체크박스 요소의 id를 fullcode 변수의 값으로 설정
+                            checkbox.value = code;  // 체크박스 요소의 value code 변수의 값으로 설정
+                            checkbox.className = 'marker-checkbox'; // 체크박스 요소에 marker-checkbox 클래스 추가
+                            var label = document.createElement('label');    // 새로운 라벨 (label) 요소 생성
+                            label.htmlFor = fullCode;   // 라벨 요소의 htmlFot 속성을 체크박스의 id와 동일한 fullcode 로 설정
+                            label.appendChild(document.createTextNode(code));   // 라벨 요소에 텍스트 노드(표시된 텍스트)를 추가. -> 이 텍스트는 code 변수 값
+
+                            listItem.appendChild(checkbox); // 리스트 항복 (li) 요소에 체크박스 요소를 자식 요소로 추가
+                            listItem.appendChild(label);    // 리스트 항복 (li) 요소에 라벨 요소를 자식 요소로 추가
+                            checklist.appendChild(listItem);    // 기존의 체크리스트(ul or ol 요소)인 checklist 에 새로 만든 리스트 항목(li)을 자식 요소로 추가
+
+                            // 체그박스 상태 변경 시 마커의 색상 업데이트
+                            checkbox.addEventListener('change', function(e) {
+                                updateSelectedMarkers(e.target.checked, fullCode, marker);
+                                updateParentCheckboxes();
+                            });
+
+                            // 마커 클릭 시 체크박스 상태 변경
+                            marker.on('click', function() {
+                                var checkbox = document.getElementById(fullCode);
+                                checkbox.checked = !checkbox.checked;
+                                updateSelectedMarkers(checkbox.checked, fullCode, marker);
+                                updateParentCheckboxes();
+                            });
+                        });
+                    } else {
                         // 상위 체크리스트 항목 : 2021_100 리스트 요소 가져옴
                         var parentChecklist = document.getElementById(folder + '_list');
 
@@ -104,58 +156,7 @@ function addMarkers(folder, subfolder, siteFile) {
                                 updateParentCheckboxes();
                             });
                         });
-                    } else {
-                        // 2021_100 외의 폴더에 대한 처리
-                        var checklist = document.getElementById(folder + '_list');
-
-                        markersData.forEach(function(markerData, index) {
-                            var lat = parseFloat(markerData.latitude);  // 위도
-                            var lng = parseFloat(markerData.longitude); // 경도
-                            var code = markerData.code || markerData.Code || markerData['Marker Code']; 
-                            var fullCode = folder + '+' + code; // 마커 이름 출력할 때 사용 , 데이터 이름 구분짓기 위해
-
-                            // 유효하지 않은 데이터 -> 건너뜀
-                            if (!lat || !lng || !code) {
-                                console.error('Invalid data: ', markerData);
-                                return;
-                            }
-
-                            // 마커 생성 -> 지도 추가
-                            var marker = L.marker([lat, lng], { 
-                                icon: icons[folder],    // 폴더 안의 아이콘 사용
-                                defaultIcon: icons[folder] // 기본 아이콘 저장
-                            }).addTo(map);
-                            markers[fullCode] = marker; // 마커를 markers 객체에 저장
-
-                            // 체크리스트 항목 생성
-                            var listItem = document.createElement('li');
-                            var checkbox = document.createElement('input');
-                            checkbox.type = 'checkbox';
-                            checkbox.id = fullCode;
-                            checkbox.value = code;
-                            checkbox.className = 'marker-checkbox';
-                            var label = document.createElement('label');
-                            label.htmlFor = fullCode;
-                            label.appendChild(document.createTextNode(code));
-
-                            listItem.appendChild(checkbox);
-                            listItem.appendChild(label);
-                            checklist.appendChild(listItem);
-
-                            // 체그박스 상태 변경 시 마커의 색상 업데이트
-                            checkbox.addEventListener('change', function(e) {
-                                updateSelectedMarkers(e.target.checked, fullCode, marker);
-                                updateParentCheckboxes();
-                            });
-
-                            // 마커 클릭 시 체크박스 상태 변경
-                            marker.on('click', function() {
-                                var checkbox = document.getElementById(fullCode);
-                                checkbox.checked = !checkbox.checked;
-                                updateSelectedMarkers(checkbox.checked, fullCode, marker);
-                                updateParentCheckboxes();
-                            });
-                        });
+                        
                     }
                 }
             });
